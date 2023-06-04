5696d26fe12118df7dfe3e28845f17a91fc94c6f
Guss-Gustavo-Couto <gustavo.emanuel.couto@gmail.com>
2023-06-04 20:30:09 +0100

    ghfjhgfj

--- routes/gallery.routes.js
+++ routes/gallery.routes.js
@@ -15,7 +15,21 @@ const Gallery = require('../models/Gallery.model');
 router.get('/gallery', async(req,res)=>{
     try{
         let allSites = await Gallery.find();
-        res.json(allSites);
+        
+        let ratingAverage = 0;
+        let ratingLength = 0;
+        for (let i = 0; i < allSitesFromDb.length; i++) {
+          for (let j = 0; j < allSitesFromDb[i].reviews.length; j++) {
+            ratingAverage += allSitesFromDb[i].reviews[j].rating;
+            ratingLength++;
+          }
+  
+          allSitesFromDb[i]["average"] = Math.round(ratingAverage / ratingLength);
+          ratingAverage = 0;
+          ratingLength = 0;
+        }
+
+        res.json(allSites, average);
     }
     catch(error){
         res.json(error);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,8 +4,22 @@ const router = express.Router();
 // Require Cloudinary
 // const fileUploader = require("../config/cloudinary.config");
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
+// Require Mongoose
+const mongoose = require('mongoose');
+
+// Require Models
+const Gallery = require('../models/Gallery.model');
+
+router.get("/", async (req, res, next) => {
+
+  const allGallery = await Gallery.find();
+  const latestGallery = allGallery.sort((a, b) => b.createdAt - a.createdAt);
+  const new1 = latestPhotos[0];
+  const new2 = latestPhotos[1];
+  const new3 = latestPhotos[2];
+  const new4 = latestPhotos[3];
+
+  res.json(latestPhotos);
+}); 
 
 module.exports = router;
