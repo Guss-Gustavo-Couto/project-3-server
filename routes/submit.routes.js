6bcb0043f6d5e43d1cfaf87803f2164981d04dfa
Guss-Gustavo-Couto <gustavo.emanuel.couto@gmail.com>
2023-06-05 10:03:01 +0100

    segunda de manha

--- routes/submit.routes.js
+++ routes/submit.routes.js
@@ -9,7 +9,21 @@ const mongoose = require('mongoose');
 const Gallery = require('../models/Gallery.model');
 
 // Require Cloudinary
-// const fileUploader = require("../config/cloudinary.config");
+const fileUploader = require("../config/cloudinary.config");
+
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/submit", fileUploader.single("imageUrl"), (req, res, next) => {
+    // console.log("file is: ", req.file)
+    if (!req.file) {
+      next(new Error("No file uploaded!"));
+      return;
+    }
+    
+    // Get the URL of the uploaded file and send it as a response.
+    // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+    res.json({ fileUrl: req.file.path });
+  });
 
 
 // POST ROUTE that Creates a new Inutil Site
