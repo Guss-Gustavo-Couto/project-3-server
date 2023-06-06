621a5d2a985737b5fff2060c174bf5931f544198
Gui Dantas <guicha2002@gmail.com>
2023-06-06 11:22:21 +0100

    Updates

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -10,7 +10,21 @@ const Gallery = require("../models/Gallery.model");
 const User = require("../models/User.model");
 
 // Require Cloudinary
-// const fileUploader = require("../config/cloudinary.config");
+const fileUploader = require("../config/cloudinary.config");
+
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("image"), (req, res, next) => {
+  // console.log("file is: ", req.file)
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+  res.json({ fileUrl: req.file.path });
+});
 
 // GET Route to display all Inutil Websites
 router.get("/admin", async (req, res) => {
